65026b607b61aefe9889f8e4da5354f275e2a15f
Georgy Dzumenko <erterj05@gmail.com>
2021-08-27 17:55:31 +0300

    Master

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/App.scss';
+import {Navigation} from './blocks/Navigation'
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
